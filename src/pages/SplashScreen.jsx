2aab3c314334be49e80042a8baa220e909a12250
KanittaJHA <kanittaketin@gmail.com>
2025-05-31 23:17:25 +0700

    Removed splash screen background image and updated SplashScreen.jsx

--- src/pages/SplashScreen.jsx
+++ src/pages/SplashScreen.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import BackgroundImage from "../assets/images/splash-screen-bg.avif";
 import SplashScreenComponent from "../components/04-splash-screen/SplashScreenComponent";
 
 const SplashScreen = () => {
@@ -24,7 +23,7 @@ const SplashScreen = () => {
   return (
     <div className="relative min-h-screen overflow-hidden">
       <img
-        src={BackgroundImage}
+        src="https://res.cloudinary.com/dvhhq4hhh/image/upload/v1748708105/splash-screen-bg_dpoitf.avif"
         alt=""
         className="fixed top-0 left-0 w-full h-full object-cover z-[-2]"
       />
